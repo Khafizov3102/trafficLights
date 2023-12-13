eaf11f67a58a9d83afe424531540209f24e96e47
Khafizov <khafizov.den@yandex.ru>
2023-12-13 22:15:30 +0400

    Initial Commit

---
+++ trafficLights/trafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  trafficLightsApp.swift
+//  trafficLights
+//
+//  Created by Денис Хафизов on 13.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct trafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
